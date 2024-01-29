d5c73202e48efc3bbdef983943286ad61afc1f9e
minji856 <sistar96@gmail.com>
2024-01-30 01:45:17 +0900

    매개 변수의 개수를 모를 경우 배열사용

--- HongongJava/src/main/java/sec04/exam02/ComputerExample.java
+++ HongongJava/src/main/java/sec04/exam02/ComputerExample.java
@@ -10,7 +10,11 @@ public class ComputerExample {
 		
 		int result2 = myCom.sum1(new int[] {1, 2, 3, 4, 5});
 		System.out.println("result2: " + result2);
+		
+		// 타입이 맞지 않음
+		// int result2 = myCom.sum1(1, 2, 3, 4, 5);
 
+		// sum2는 배열 선언 없이 자유롭게 가능
 		int result3 = myCom.sum2(1, 2, 3);
 		System.out.println("result3: " + result3);
 		
