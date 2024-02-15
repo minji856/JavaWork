78ae95afe6b9d3c98d8b12c30e8b9b6b44c82bde
minji856 <sistar96@gmail.com>
2024-02-15 20:57:00 +0900

    :sparkles: Getter,Setter 메소드

--- HongongJava/src/main/java/sec06/exam06/CarExample.java
+++ HongongJava/src/main/java/sec06/exam06/CarExample.java
@@ -8,5 +8,16 @@ public class CarExample {
 		myCar.setSpeed(-50);
 		
 		System.out.println("현재 속도: " + myCar.getSpeed());
+		
+		//올바른 속도 변경
+		myCar.setSpeed(60);
+		System.out.println("현재 속도: " + myCar.getSpeed());
+		
+		//멈춤
+		if(!myCar.isStop()) {
+			myCar.setStop(true);
+		}
+		
+		System.out.println("현재 속도: " + myCar.getSpeed());
 	}
 }
