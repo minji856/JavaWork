de525379fca986d976d28cf19ac23b40167d2086
minji856 <sistar96@gmail.com>
2024-02-21 13:43:01 +0900

    :sparkles: 기본 생성자가 없는 부모 클래스

---
+++ HongongJava/src/main/java/sec01/exam02/People.java
@@ -0,0 +1,12 @@
+package sec01.exam02;
+
+public class People {
+	public String name;
+	public String ssn;
+	
+	//기본 생성자가 없고 매개값만 있는 객체 생성자
+	public People(String name, String ssn) {
+		this.name = name;
+		this.ssn = ssn;
+	}
+}
