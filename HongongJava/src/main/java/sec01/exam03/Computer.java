e7e34c01f27094419b18566b8241adf1f95eaf22
minji856 <sistar96@gmail.com>
2024-02-23 17:19:47 +0900

    :sparkles: 오버라이딩 자식 클래스

--- HongongJava/src/main/java/sec01/exam03/Computer.java
+++ HongongJava/src/main/java/sec01/exam03/Computer.java
@@ -3,7 +3,7 @@ package sec01.exam03;
 public class Computer extends Calculator{
 	@Override // 메소드 이름이 다르면 컴파일 에러가 발생함 areaCircl(X)
 	double areaCircle(double r) {
-		System.out.println("Calculator 객체의 areaCircle() 실행");
+		System.out.println("Computer 객체의 areaCircle() 실행");
 		return Math.PI * r * r; // 자바 표준 API로 재정의
 	}
 }
